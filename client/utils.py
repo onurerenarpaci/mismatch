f79030eabe64fdd6ca36ae5b3b4ab2bf2b40ae35
Onur Eren Arpaci <onurerenarpaci@hotmail.com>
2022-08-16 22:50:06 +0300

    add comments for utils and renamed mismatch.sol

--- client/utils.py
+++ client/utils.py
@@ -4,6 +4,10 @@ import sha3
 from eth_abi.abi import encode_abi
 
 def translate_proof(mt_ins, x):
+    """
+    Given a merkle tree and a leaf index, return the proof
+    It also includes a boolean array to indicate wheter the corresponding node is a left node or a right node
+    """
     root = '0x' + mt_ins.get_merkle_root()
     leaf = '0x' + mt_ins.get_leaf(x)
     res_proof = []
@@ -18,10 +22,16 @@ def translate_proof(mt_ins, x):
     return {"root":root, "leaf":leaf, "arr":res_proof, "pos":res_bool}
 
 def hash_op(PC, op):
+    """
+    Given PC and op, return the hash of the op
+    """
     raw = [PC, int(op['OpCode']), op['args']]
     return bytes(Web3.solidityKeccak(['int32', 'int32', 'int32[]'], raw)).hex()
 
 def code_mt(codes):
+    """
+    Given a list of codes, return the merkle tree of the codes
+    """
     mt = merkletools.MerkleTools(hash_type="keccak_256")
     leaves = [hash_op(PC, op) for PC, op in enumerate(codes)]
     mt.add_leaf(leaves)
@@ -29,9 +39,16 @@ def code_mt(codes):
     return mt
 
 def make_store_leaf(idx, x):
+    """
+    Given an index and a value, return the leaf of the merkle tree
+    The hash includes the index because we need to prove that the paticular value is stored at that index
+    """
     return bytes(Web3.solidityKeccak(['int32', 'int32'], [idx, x.item()])).hex()
 
 def store_mt(store):
+    """
+    Given a store, return the merkle tree of the store
+    """
     mt = merkletools.MerkleTools(hash_type="keccak_256")
     leaves = [make_store_leaf(idx, x) for idx, x in enumerate(store)]
     mt.add_leaf(leaves)
@@ -39,13 +56,22 @@ def store_mt(store):
     return mt
 
 def hash_rc(rc):
+    """
+    Given a reduced configuration, return the hash of the reduced configuration
+    """
     raw = [rc['PC'], int(rc['OpCode']), rc['args'], rc['store_mt_root']]
     return bytes(Web3.solidityKeccak(['int32', 'int32', 'int32[]', 'bytes32'], raw)).hex()
 
 def sterilize_ints(ints):
+    """
+    Given a list that possibly contains numpy types, return a list of ints
+    """
     return list(map(lambda x: x if (type(x) is int) else x.item(), ints))
 
 def reduced_config_mt(rc):
+    """
+    Given a list of reduced configurations, return the merkle tree of the reduced configurations
+    """
     mt = merkletools.MerkleTools(hash_type="keccak_256")
     leaves = list(map(hash_rc, rc))
     mt.add_leaf(leaves)
